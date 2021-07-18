0f996089f83a916e738f3de2e8d8f3a01efa32ca
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-07-19 00:27:44 +0900

    Add files via upload

---
+++ static/js/covid.js
@@ -0,0 +1,64 @@
+function confirm() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            img_name = this.responseText
+            document.getElementById("view").innerHTML = '<img src="./static/image/confirm.jpg" width=500 height=400>'
+        }
+    };
+    xhttp.open("POST", "confirm");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+}
+
+function death() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            img_name = this.responseText
+            document.getElementById("view").innerHTML = '<img src="./static/image/death.jpg" width=500 height=400>'
+        }
+    };
+    xhttp.open("POST", "death");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+}
+
+function vaccine() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            img_name = this.responseText
+            document.getElementById("view").innerHTML = '<img src="./static/image/vaccine.jpg" width=500 height=400>'
+        }
+    };
+    xhttp.open("POST", "vaccine");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+}
+
+function gdp() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            img_name = this.responseText
+            document.getElementById("view").innerHTML = '<img src="./static/image/gdp.jpg" width=500 height=400>'
+        }
+    };
+    xhttp.open("POST", "gdp");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+}
+
+function population() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            img_name = this.responseText
+            document.getElementById("view").innerHTML = '<img src="./static/image/population.jpg" width=500 height=400>'
+        }
+    };
+    xhttp.open("POST", "population");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+}
