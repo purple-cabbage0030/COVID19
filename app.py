9ea14ae1324073d591cf501d3269286b25ed682d
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-07-19 00:26:42 +0900

    워드클라우드 함수 추가

--- app.py
+++ app.py
@@ -20,23 +20,67 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/', methods=['get'])
 def index():
-    return render_template('index.html')
+    return render_template('index_02.html')
 
 
 @app.route('/covidchart', methods=['get'])
 def get_covid():
     return ""
 
-@app.route('/visual', methods=['post'])
-def visual():
+@app.route('/confirm', methods=['post'])
+def confirm():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
     for i in range(len(conf)):
         confirm_visual[conf.iloc[i,0]] = conf.iloc[i,1]
     stylecloud.gen_stylecloud(text=confirm_visual,icon_name="fas fa-certificate",
                         palette="colorbrewer.diverging.Spectral_11",background_color='black',
-                        gradient="horizontal",output_name="test.png")
-    Image.open("test.png").show()
+                        gradient="horizontal",output_name="confirm.jpg")
+    return "confirm.jpg"
+
+@app.route('/death', methods=['post'])
+def death():
+    confirm_visual = {}
+    conf = pd.read_csv('merge_vaccine.csv')
+    for i in range(len(conf)):
+        confirm_visual[conf.iloc[i,0]] = conf.iloc[i,3]
+    stylecloud.gen_stylecloud(text=confirm_visual,icon_name="fas fa-skull-crossbones",
+                        palette="colorbrewer.diverging.Spectral_11",background_color='black',
+                        gradient="horizontal",output_name="death.jpg")
+    return "death.jpg"
+
+@app.route('/vaccine', methods=['post'])
+def vaccine():
+    confirm_visual = {}
+    conf = pd.read_csv('merge_vaccine.csv')
+    for i in range(len(conf)):
+        confirm_visual[conf.iloc[i,0]] = conf.iloc[i,13]
+    stylecloud.gen_stylecloud(text=confirm_visual,icon_name="fas fa-thermometer",
+                        palette="colorbrewer.diverging.Spectral_11",background_color='black',
+                        gradient="horizontal",output_name="vaccine.jpg")
+    return "vaccine.jpg"
+
+@app.route('/gdp', methods=['post'])
+def gdp():
+    confirm_visual = {}
+    conf = pd.read_csv('merge_vaccine.csv')
+    for i in range(len(conf)):
+        confirm_visual[conf.iloc[i,0]] = conf.iloc[i,9]
+    stylecloud.gen_stylecloud(text=confirm_visual,icon_name="fas fa-money-bill-wave-alt",
+                        palette="colorbrewer.diverging.Spectral_11",background_color='black',
+                        gradient="horizontal",output_name="gdp.jpg")
+    return "gdp.jpg"
+
+@app.route('/population', methods=['post'])
+def population():
+    confirm_visual = {}
+    conf = pd.read_csv('merge_vaccine.csv')
+    for i in range(len(conf)):
+        confirm_visual[conf.iloc[i,0]] = conf.iloc[i,8]
+    stylecloud.gen_stylecloud(text=confirm_visual,icon_name="fas fa-certificate",
+                        palette="colorbrewer.diverging.Spectral_11",background_color='black',
+                        gradient="horizontal",output_name="population.jpg")
+    return "population.jpg"
 
 
 if __name__ == '__main__':
