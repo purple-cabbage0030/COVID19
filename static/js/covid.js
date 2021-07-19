62ba64539541cb07c83250dedc98fe5abb719607
Hongju Lee <litehj84@gmail.com>
2021-07-19 18:09:55 +0900

    Update covid.js

--- static/js/covid.js
+++ static/js/covid.js
@@ -2,6 +2,8 @@ function confirm() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chart").style.display = 'none';
+            document.getElementById("view").style.display = 'block';
             img_name = this.responseText
             document.getElementById("view").innerHTML = '<img src="./static/img/confirm.jpg" width=500 height=400>'
         }
@@ -15,6 +17,8 @@ function death() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chart").style.display = 'none';
+            document.getElementById("view").style.display = 'block';
             img_name = this.responseText
             document.getElementById("view").innerHTML = '<img src="./static/img/death.jpg" width=500 height=400>'
         }
@@ -28,6 +32,8 @@ function vaccine() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chart").style.display = 'none';
+            document.getElementById("view").style.display = 'block';
             img_name = this.responseText
             document.getElementById("view").innerHTML = '<img src="./static/img/vaccine.jpg" width=500 height=400>'
         }
@@ -41,6 +47,8 @@ function gdp() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chart").style.display = 'none';
+            document.getElementById("view").style.display = 'block';
             img_name = this.responseText
             document.getElementById("view").innerHTML = '<img src="./static/img/gdp.jpg" width=500 height=400>'
         }
@@ -54,6 +62,8 @@ function population() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chart").style.display = 'none';
+            document.getElementById("view").style.display = 'block';
             img_name = this.responseText
             document.getElementById("view").innerHTML = '<img src="./static/img/population.jpg" width=500 height=400>'
         }
@@ -69,6 +79,7 @@ function chart_view() {
         if (this.readyState == 4 && this.status == 200) {
             img_name = this.responseText
             document.getElementById("view").style.display = 'none';
+            document.getElementById("chart").style.display = 'block';
             document.getElementById("view1").innerHTML = '<img src="./static/img/covid_gdp_plot1.png" width=95%>'
             document.getElementById("view2").innerHTML = '<img src="./static/img/covid_gdp_plot2.png" width=95%>'
             document.getElementById("view3").innerHTML = '<img src="./static/img/covid_gdp_plot3.png" width=95%>'
