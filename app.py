20f549322854151c4396605af9f5c0e70c22eaa7
Hongju Lee <litehj84@gmail.com>
2021-07-19 18:11:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-from selenium import webdriver 
-from bs4 import BeautifulSoup 
-import time
+from crawler import Crawling
+import platform
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
-import numpy as np
-import platform
-import requests
-import pyautogui
-import os
-from openpyxl import load_workbook
 from wordcloud import WordCloud
 from PIL import Image
 import stylecloud
@@ -21,14 +15,13 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/', methods=['get'])
 def index():
-    return render_template('index_02.html')
-
+    return render_template('index.html')
 
 @app.route('/covidchart', methods=['get'])
 def get_covid():
     return ""
 
-@app.route('/confirm', methods=['post'])
+@app.route('/confirm', methods=['post', 'get'])
 def confirm():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
@@ -39,7 +32,7 @@ def confirm():
                         gradient="horizontal",output_name="./static/img/confirm.jpg")
     return "confirm.jpg"
 
-@app.route('/death', methods=['post'])
+@app.route('/death', methods=['post', 'get'])
 def death():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
@@ -50,7 +43,7 @@ def death():
                         gradient="horizontal",output_name="./static/img/death.jpg")
     return "death.jpg"
 
-@app.route('/vaccine', methods=['post'])
+@app.route('/vaccine', methods=['post', 'get'])
 def vaccine():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
@@ -61,7 +54,7 @@ def vaccine():
                         gradient="horizontal",output_name="./static/img/vaccine.jpg")
     return "vaccine.jpg"
 
-@app.route('/gdp', methods=['post'])
+@app.route('/gdp', methods=['post', 'get'])
 def gdp():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
@@ -72,7 +65,7 @@ def gdp():
                         gradient="horizontal",output_name="./static/img/gdp.jpg")
     return "gdp.jpg"
 
-@app.route('/population', methods=['post'])
+@app.route('/population', methods=['post', 'get'])
 def population():
     confirm_visual = {}
     conf = pd.read_csv('merge_vaccine.csv')
@@ -83,13 +76,13 @@ def population():
                         gradient="horizontal",output_name="./static/img/population.jpg")
     return "population.jpg"
 
-@app.route("/covidgdp", methods=["post"])
+@app.route("/covidgdp", methods=['post'])
 def covid_gdp():
     Visual.visual1()
     Visual.visual2()
     Visual.visual3()
     return ''
 
+
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1", port=5000)
-    # gdp()
